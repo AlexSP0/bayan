b2cdd8cc9bd3b7f1bb12528d55cb51e8138ae8f5
Alexey Saprunov <workernight@yandex.ru>
2023-05-12 11:20:38 +0400

    chore: implement MD5Hash class

--- hash/md5hash.cpp
+++ hash/md5hash.cpp
@@ -1,6 +1,21 @@
-#include <md5hash.h>
+#include <boost/uuid/detail/md5.hpp>
+
+#include "md5hash.h"
 
 std::string MD5Hash::hashAsString(const char *data, size_t size)
 {
-    return "";
+    std::string res;
+
+    boost::uuids::detail::md5 hash;
+    boost::uuids::detail::md5::digest_type digest;
+
+    hash.process_bytes(data, size);
+    hash.get_digest(digest);
+
+    for (int i = 0; i < 4; i++)
+    {
+        res += std::to_string(digest[i]);
+    }
+
+    return res;
 }
