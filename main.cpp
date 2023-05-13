c5fd724c1e271136b0e79bbf4fa0f1afb6834067
Alexey Saprunov <workernight@yandex.ru>
2023-05-13 16:21:24 +0400

    chore: implement main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
+#include "options/clparser.h"
+
+#include "filefinder.h"
+#include "handler.h"
 
 int main(int argc, char *argv[])
 {
+    std::shared_ptr<IOptions> options = CLParser().getOptions(argc, argv);
+
+    if (!options)
+    {
+        return 0;
+    }
+
+    std::shared_ptr<Handler> handler = std::make_shared<Handler>(options);
+
+    FileFinder finder(options);
+
+    finder.scan();
+    finder.out(handler);
+
     return 0;
 }
